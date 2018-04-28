71a52742303479cce3f4a4ac40736393bbf0fa77
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2018-04-27 21:15:35 -0400

    main function test

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+
+	"fmt"
+)
+
+
+func main() {
+
+	bc := NewBlockChain()	
+
+	bc.AddBlock("Send 1 BTC to Raseshwari")
+	bc.AddBlock("Send 1 BTC to prakhar")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()	
+	}
+}
